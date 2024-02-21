e918049a677faf65a64784fae14ce62fd3f4bd90
unknown <aalluhaybi@una.edu>
2024-02-21 16:45:34 -0600

    last update

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
+require('dotenv').config();
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000 ; 
+const port = process.env.PORT || 5500;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyParser = require('body-parser');
 
-
+// Set the view engine to ejs
 let path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Define the type string
 let myTypeServer = "🌟 Dreaming of Graduation 🎓";
 
-//const mongoURI = process.env.URI;
-require('dotenv').config();
-
-const mongoURI = process.env.URI;
-const client = new MongoClient(mongoURI, {
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -22,39 +23,53 @@ const client = new MongoClient(mongoURI, {
   }
 });
 
+// Define an asynchronous function to connect to MongoDB and fetch data
 async function run() {
   try {
     await client.connect();
     const result = await client.db("lab_papa-database").collection("papa-collection").find().toArray();
-    console.log("cxnDB result: ", result);
-    console.log("Oinged your deployment. you successfully connected to MongoDB!");
-
+    return result;
   } finally {
-
     await client.close();
   }
-
 }
 
+// Define a route to handle reading data from MongoDB
+app.get('/read', async (req,res) => {
+
+  let myResultServer = await run(); 
+
+  console.log("myResultServer:", myResultServer[0].userName);
+
+
+
+  res.render('index', {
+    myTypeClient: myTypeServer,
+    myResultClient: null
+
+  });
+
+
+}); 
 run().catch(console.dir);
 
-app.get('/', function (req, res) {
-  res.render('index', {
-    myTypeClient: myTypeServer
-  });
-});
 
-app.get('/send', function (req, res) {
+
+app.get('/', function(req, res) {
+
+  res.render('index', {
+   
+    myTypeClient: myTypeServer 
+
+  });
+  
+});
+// Define a route to send a simple response
+app.get('/send', (req, res) => {
   res.send('Hello World from Express <br><a href="/">home</a>');
 });
 
-app.get('/source-code', function (req, res) {
-  
-  res.send('Source code goes here');
-});
-
+// Start the server
 app.listen(port, () => {
   console.log(`papa app listening on port ${port}`);
 });
-
-
