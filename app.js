ad6d7b77bf4ae08b988394b3770395782b86ab8c
unknown <aalluhaybi@una.edu>
2024-02-22 07:07:58 -0600

    .

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5500;
+const port = process.env.PORT || 5501;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
 
@@ -12,8 +11,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Define the type string
-let myTypeServer = "🌟 Dreaming of Graduation 🎓";
+// Initialize myTypeServer and myResultClient
+let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
+let myResultClient = [];
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(process.env.URI, {
@@ -36,37 +36,34 @@ async function run() {
 }
 
 // Define a route to handle reading data from MongoDB
-app.get('/read', async (req,res) => {
-
-  let myResultServer = await run(); 
-
-  console.log("myResultServer:", myResultServer[0].userName);
-
-
-
+app.get('/read', async (req, res) => {
+  myResultClient = await run();
+  console.log("myResultServer:", myResultClient);
   res.render('index', {
     myTypeClient: myTypeServer,
-    myResultClient: null
-
+    myResultClient: myResultClient
   });
-
-
-}); 
-run().catch(console.dir);
-
-
-
-app.get('/', function(req, res) {
-
-  res.render('index', {
-   
-    myTypeClient: myTypeServer 
-
-  });
-  
 });
+
+// Define the home route
+app.get('/', function (req, res) {
+  res.render('index', {
+    myTypeClient: myTypeServer
+  });
+});
+
+// Define a route to handle changing the type
+app.get('/name', (req, res) => {
+  console.log("in get to slash name:", req.query.ejsFormName);
+  myTypeServer = req.query.ejsFormName;
+  res.render('index', {
+    myTypeClient: myTypeServer,
+    myResultClient: myResultClient
+  });
+});
+
 // Define a route to send a simple response
-app.get('/send', (req, res) => {
+app.get('/send', function (req, res) {
   res.send('Hello World from Express <br><a href="/">home</a>');
 });
 
@@ -74,3 +71,4 @@ app.get('/send', (req, res) => {
 app.listen(port, () => {
   console.log(`papa app listening on port ${port}`);
 });
+
