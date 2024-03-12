ffcb358a06678ec230ba5dda5c2cf882c8603dde
unknown <aalluhaybi@una.edu>
2024-03-12 01:41:39 -0500

    update

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const bodyParser = require('body-parser')
-//const uri = "mongodb+srv://sbasalim:Saleh123@cluster0.h1oabai.mongodb.net/?retryWrites=true&w=majority";
 
 // set the view engine to ejs
 let path = require('path');
@@ -17,11 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 let myTypeServer = "9️⃣ The Peacemaker ✌🏻";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+const uri = process.env.URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  if (err) {
+    console.error('Failed to connect to MongoDB', err);
+  } else {
+    console.log('Connected to MongoDB');
   }
 });
 
@@ -31,7 +33,7 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    const result = await client.db("papa-database").collection("papa-collection").find().toArray();
+    const result = await client.db("papa_database").collection("papa_collection").find().toArray();
 
     //console.log("cxnDB result: ", result);
     return result; 
